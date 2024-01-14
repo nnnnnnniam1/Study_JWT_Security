3819fadf0756c07efd843b81feab6fd46db0f73a
nnnnnnniam1 <nnnnnnniam1@gmail.com>
2024-01-15 00:06:22 +0900

    프로젝트 연결

---
+++ src/main/java/me/naayeah/jwt/tutorial/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package me.naayeah.jwt.tutorial.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig {
+}
